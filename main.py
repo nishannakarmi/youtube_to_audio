564e339b292aa3d85c500795ac0d56b42b7e68af
Nishan Nakarmi <nishannakarmi@gmail.com>
2016-06-09 17:43:15 +0545

    added settings file

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 from flask import Flask, render_template, request, redirect, url_for
 import youtube_dl
 from utils import get_video_info
+from settings import MAXIMUM_VIDEO_TIME, ERROR_MSG
 
 app = Flask(__name__)
 # app.run(debug=True)
@@ -11,36 +12,55 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-
-@app.route('/process', methods=['POST', 'GET'])
-def process_file():
-    error = None
+@app.route('/pre_process', method=['POST'])
+def pre_process():
     if request.method == 'POST':
-        print request.form.getlist('url')
         urls = request.form.getlist('url')
         if len(urls):
             for url in urls:
                 info = get_video_info(url)
-                if info and info['duration'] <= 360:
-                    download_url = [url]
-                    ydl_opts = {
-                        'format': 'bestaudio/best',
-                        'postprocessors': [{
-                            'key': 'FFmpegExtractAudio',
-                            'preferredcodec': 'mp3',
-                            'preferredquality': '192',
-                        }],
-                        'outtmpl': 'tmp/%(title)s-%(id)s.%(ext)s',
-                    }
-                    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                        ydl.download(download_url)
+                if info:
+                    pass
                 else:
-                    print("LARGE VIDEO OR UNKNOW ERROR")
+                    pass
+
+
+@app.route('/process', methods=['POST'])
+def process_file():
+    status = {}
+    if request.method == 'POST':
+        print request.form.getlist('url')
+        urls = request.form.getlist('url')
+        if len(urls):
+            counter = 0
+            for url in urls:
+                counter += 1
+                info = get_video_info(url)
+                status[counter] = {'success': False}
+                if info:
+                    status[counter]['title'] = info['title'] if 'title' in info else ''
+                    if info['duration'] <= MAXIMUM_VIDEO_TIME:
+                        download_url = [url]
+                        ydl_opts = {
+                            'format': 'bestaudio/best',
+                            'postprocessors': [{
+                                'key': 'FFmpegExtractAudio',
+                                'preferredcodec': 'mp3',
+                                'preferredquality': '192',
+                            }],
+                            'outtmpl': 'tmp/%(title)s-%(id)s.%(ext)s',
+                        }
+                        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                            ydl.download(download_url)
+                            status[counter]['success'] = True
+                    else:
+                        if info['duration'] > 360:
+                            status[counter]['msg'] = ERROR_MSG['MAX_VIDEO_TIME']
+                else:
+                    status['counter']['msg'] = ERROR_MSG['UNKNOWN']
+                print status
 
             return render_template('process_file.html')
-        else:
-            error = 'Invalid Url found'
-            return redirect(url_for('home'))
 
     return redirect(url_for('home'))
 
---
+++ settings.py
@@ -0,0 +1,5 @@
+MAXIMUM_VIDEO_TIME = 360
+ERROR_MSG = {
+    'MAX_VIDEO_TIME': 'This video cannot be processed, it time is greater than %s min' %(MAXIMUM_VIDEO_TIME/60),
+    'UNKNOWN': 'Sorry Unknown error arises'
+}
