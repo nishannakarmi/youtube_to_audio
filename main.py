23b0ef0ba3ce02fa9424be1b7192b61fa6daf616
Nishan Nakarmi <nishannakarmi@gmail.com>
2016-06-10 18:15:32 +0545

    basic design in progress

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from __future__ import unicode_literals
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash, session, escape, jsonify
 import youtube_dl
 from utils import get_video_info
-from settings import MAXIMUM_VIDEO_TIME, ERROR_MSG, SECRET_KEY
+from settings import MAXIMUM_VIDEO_TIME, ERROR_MSG, SECRET_KEY, MAXIMUM_NUMBER_OF_VIDEOS
+import uuid
 
 app = Flask(__name__)
 app.debug = True
@@ -11,59 +12,90 @@ app.secret_key = SECRET_KEY
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', max_videos=MAXIMUM_NUMBER_OF_VIDEOS)
 
-# @app.route('/pre_process', method=['POST'])
-# def pre_process():
-#     if request.method == 'POST':
-#         urls = request.form.getlist('url')
-#         if len(urls):
-#             for url in urls:
-#                 info = get_video_info(url)
-#                 if info:
-#                     pass
-#                 else:
-#                     pass
+
+@app.route('/pre_process', methods=['POST'])
+def pre_process():
+    if request.method == 'POST':
+        urls = request.form.getlist('url')
+        custom_uuid = None
+        if len(urls):
+            error_msg = []
+            for url in urls:
+                info = get_video_info(url)
+                if info and info['duration'] <= MAXIMUM_VIDEO_TIME:
+                    continue
+                else:
+                    if not info:
+                        error_msg.append(ERROR_MSG['UNKNOWN'])
+                    else:
+                        error_msg.append(ERROR_MSG['MAX_VIDEO_TIME'])
+            if len(error_msg):
+                flash("\n".join(error_msg))
+                return redirect(url_for('home'))
+            else:
+                custom_uuid = uuid.uuid4()
+                session['urls'] = urls
+                session['uuid'] = custom_uuid
+        else:
+            flash("Please insert youtube video url's")
+            return redirect(url_for('home'))
+        return render_template('pre_process.html', custom_uuid=custom_uuid)
 
 
 @app.route('/process', methods=['POST'])
 def process_file():
-    status = {}
     if request.method == 'POST':
-        print request.form.getlist('url')
-        urls = request.form.getlist('url')
-        if len(urls):
-            counter = 0
-            for url in urls:
-                counter += 1
-                info = get_video_info(url)
-                status[counter] = {'success': False}
-                if info:
-                    status[counter]['title'] = info['title'] if 'title' in info else ''
-                    if info['duration'] <= MAXIMUM_VIDEO_TIME:
-                        download_url = [url]
-                        ydl_opts = {
-                            'format': 'bestaudio/best',
-                            'postprocessors': [{
-                                'key': 'FFmpegExtractAudio',
-                                'preferredcodec': 'mp3',
-                                'preferredquality': '192',
-                            }],
-                            'outtmpl': 'tmp/%(title)s-%(id)s.%(ext)s',
-                        }
-                        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-                            ydl.download(download_url)
-                            status[counter]['success'] = True
-                    else:
-                        if info['duration'] > 360:
-                            status[counter]['msg'] = ERROR_MSG['MAX_VIDEO_TIME']
-                else:
-                    status['counter']['msg'] = ERROR_MSG['UNKNOWN']
-                print status
+        custom_uuid = request.form['custom_uuid']
+        success = False
 
-            return render_template('process_file.html')
+        if custom_uuid and (str(custom_uuid) == str(session.get('uuid', None))):
+            success = True
+            msg = 'It Works Cheers!'
+        else:
+            msg = ERROR_MSG['UNKNOWN']
 
-    return redirect(url_for('home'))
+        return jsonify(success=success, custom_uuid=custom_uuid, msg=msg)
+    else:
+        flash("Unauthorized access")
+        return redirect(url_for('home'))
+    # status = {}
+    # if request.method == 'POST':
+    #     print request.form.getlist('url')
+    #     urls = request.form.getlist('url')
+    #     if len(urls):
+    #         counter = 0
+    #         for url in urls:
+    #             counter += 1
+    #             info = get_video_info(url)
+    #             status[counter] = {'success': False}
+    #             if info:
+    #                 status[counter]['title'] = info['title'] if 'title' in info else ''
+    #                 if info['duration'] <= MAXIMUM_VIDEO_TIME:
+    #                     download_url = [url]
+    #                     ydl_opts = {
+    #                         'format': 'bestaudio/best',
+    #                         'postprocessors': [{
+    #                             'key': 'FFmpegExtractAudio',
+    #                             'preferredcodec': 'mp3',
+    #                             'preferredquality': '192',
+    #                         }],
+    #                         'outtmpl': 'tmp/%(title)s-%(id)s.%(ext)s',
+    #                     }
+    #                     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    #                         ydl.download(download_url)
+    #                         status[counter]['success'] = True
+    #                 else:
+    #                     if info['duration'] > 360:
+    #                         status[counter]['msg'] = ERROR_MSG['MAX_VIDEO_TIME']
+    #             else:
+    #                 status['counter']['msg'] = ERROR_MSG['UNKNOWN']
+    #             print status
+    #
+    #         return render_template('process_file.html')
+    #
+    # return redirect(url_for('home'))
 
 
 @app.route('/download')
--- settings.py
+++ settings.py
@@ -1,7 +1,8 @@
 MAXIMUM_VIDEO_TIME = 360
+MAXIMUM_NUMBER_OF_VIDEOS = 4
 ERROR_MSG = {
     'MAX_VIDEO_TIME': 'This video cannot be processed, it time is greater than %s min' %(MAXIMUM_VIDEO_TIME/60),
-    'UNKNOWN': 'Sorry Unknown error arises'
+    'UNKNOWN': 'Sorry Unknown Error'
 }
 """
 secrete key generation method
